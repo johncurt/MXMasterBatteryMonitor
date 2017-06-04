a84335dbfb54a0979584331f762aa4a0620ec94c
John Fansler <jfansler@vyasystems.com>
2017-06-04 14:49:29 -0700

    Initial Commit

---
+++ MXMasterBatteryMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MXMasterBatteryMonitor
+//
+//  Created by John Fansler on 6/4/17.
+//  Copyright © 2017 John Fansler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
